b9b71cf382a68308c6c29527639ce521b4ed9382
FalaBiu <deyvisonleite@yahoo.com.br>
2022-09-12 20:53:32 -0300

    Commit Inicial do Projeto

---
+++ Insert musica.sql
@@ -0,0 +1,87 @@
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (2, 'Ela une todas as coisas', 4.14);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (3, 'Porque eu sei que é amor', 4.14);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (4, 'Uma Arlinda Mulher', 4.01);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (5, 'Comer tatú é bom', 3.44);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (6, 'Lembrança de uma rata', 3.10);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (7, 'A barata da vizinha', 3.55);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (8, 'Cavalo Manco', 3.21);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (9, 'Woman No Cry', 5.01);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (10, 'Faroeste Caboclo', 9.20);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (11, 'Azul da cor do mar', 5.34);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (12, 'Colombina', 3.57);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (13, 'Manoel', 3.13);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (14, 'Velha infância', 3.01);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (15, 'Fenix', 5.21);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (16, 'Fogo', 4.01);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (17, 'Primeiros Erros', 4.54);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (18, 'Natasha', 4.12);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (19, 'Gira gira', 2.50);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (20, 'Faz amor comigo', 3.29);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (21, 'Nós capota mas não breca', 3.33);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (22, 'Somos dois', 4.17);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (23, 'Melhor eu ir', 5.01);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (24, 'Procuro um amor', 3.21);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (25, 'Facil', 3.30);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (26, 'Sol e Mar', 3.01);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (27, 'Segundo Sol', 4.13);
+
+insert into musica(CODIGO_MUSICA, NOME_MUSICA, DURACAO)
+values (28, 'Nega do cabelo duro', 3.43);
+
+select * from musica
+
+
+
+
+
---
+++ insert gravadoras.sql
@@ -0,0 +1,25 @@
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (1, 'BMC','Julesca Nautilus');
+
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (2, 'Universal','Uzumaki Naruto');
+
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (3, 'Sony Music','Arlinda Mulher');
+
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (4, 'Som Livre','Dr. Junelson Almerindo');
+
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (5, 'Caco de Telha','Genilson Perpetuo');
+
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (6, 'Fox Music','Michael McCartney');
+
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (7, 'ABC','Silvio Santos');
+
+insert into GRAVADORA (CODIGO_GRAVADORA, NOME_GRAVADORA, CONTATO) 
+values (8, 'BMC','Julesca Nautilus');
+
+
